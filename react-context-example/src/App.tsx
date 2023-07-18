775babd8bb96ab152d1f10177390eff52766a3cc
Maya Estalilla <mestalilla@gmail.com>
2023-07-18 13:45:21 -0400

    Demonstrate unnecessary Context API re-renders

--- react-context-example/src/App.tsx
+++ react-context-example/src/App.tsx
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { useExampleContext } from './ExampleContext'
 
-function App() {
+let appRenderedTimes = 0
+let containerRenderedTimes = 0
+let innerOneRenderedTimes = 0
+let innerTwoRenderedTimes = 0
+
+const App: React.FC = () => {
+  appRenderedTimes++
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <>
+      <div className="App Common">
+        <p className="Notes">
+          All components use their own data and setters. Unfortunately, our
+          inner components and container will re-render even if unrelated data
+          was updated.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <span>App rendered: {appRenderedTimes}</span>
+        <Container />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export const Container = () => {
+  containerRenderedTimes++
+
+  const { containerData, setContainerData } = useExampleContext()
+
+  const handleOnClick = () => {
+    setContainerData(prevState => !prevState)
+  }
+
+  return (
+    <div className="Container Common" onClick={handleOnClick}>
+      <span>
+        Container rendered: {containerRenderedTimes}
+        {`: ${containerData}`}
+      </span>
+      <InnerOne />
+      <InnerTwo />
+    </div>
+  )
+}
+
+export const InnerOne = () => {
+  innerOneRenderedTimes++
+
+  const { innerOneData, setInnerOneData } = useExampleContext()
+
+  const handleOnClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
+    setInnerOneData(prevState => !prevState)
+  }
+
+  return (
+    <div className="InnerOne Common" onClick={handleOnClick}>
+      <span>
+        Inner one rendered: {innerOneRenderedTimes}
+        {`: ${innerOneData}`}
+      </span>
+    </div>
+  )
+}
+
+export const InnerTwo = () => {
+  innerTwoRenderedTimes++
+
+  const { innerTwoData, setInnerTwoData } = useExampleContext()
+
+  const handleOnClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
+    setInnerTwoData(prevState => !prevState)
+  }
+
+  return (
+    <div className="InnerTwo Common" onClick={handleOnClick}>
+      <span>
+        Inner two rendered: {innerTwoRenderedTimes}
+        {`: ${innerTwoData}`}
+      </span>
+    </div>
+  )
+}
+
+export default App
--- react-context-example/src/index.tsx
+++ react-context-example/src/index.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
+import reportWebVitals from './reportWebVitals'
+import './index.css'
+import App from './App'
+import { ExampleContextProvider } from './ExampleContext'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
+  <ExampleContextProvider>
     <App />
-  </React.StrictMode>
-);
+  </ExampleContextProvider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
